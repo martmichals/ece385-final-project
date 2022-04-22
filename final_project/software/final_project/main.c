98d489a142794fa5a7ab8a8a52e7eea3a2125728
Martin Michalski <martmichals@gmail.com>
2022-04-21 19:10:58 -0500

    Backup

--- final_project/software/final_project/main.c
+++ final_project/software/final_project/main.c
@@ -3,11 +3,11 @@
 int main() {
 	init_color_palette();
 	draw_background();
-	draw_char(48, 48, 0);
-	draw_char(48, 10, 1);
-	draw_char(48, 100, 2);
-	draw_char(100, 100, 20);
-	draw_char(100, 40, 24);
+	// draw_char(48, 48, 0);
+	// draw_char(48, 10, 1);
+	// draw_char(48, 100, 2);
+	// draw_char(100, 100, 20);
+	draw_char(102, 40, 20);
 	usleep (1000000);
 	return 1;
 }
--- final_project/software/final_project/pixel_vga.c
+++ final_project/software/final_project/pixel_vga.c
@@ -59,42 +59,80 @@ void draw_background() {
 // TODO: Create structures for the fonts
 void draw_char(alt_u8 x, alt_u8 y, alt_u8 render_code) {
     // Get the character metadata
-    alt_u8 width = title_widths[render_code];
-    alt_u8 height = TITLE_PIXEL_HEIGHT;
-    alt_u32 char_data_idx = title_char_indices[render_code];
+    alt_u8 char_width = title_widths[render_code];
+    alt_u8 char_height = TITLE_PIXEL_HEIGHT;
+    alt_u32 font_outer = title_char_indices[render_code];
 
     // Debug
     printf("Drawing character with render code: %d\n", render_code);
-    printf("Character has height:               %d\n", height);
-    printf("Character has index into data array:%d\n", char_data_idx);
+    printf("Character has height:               %d\n", char_height);
+    printf("Character has width:                %d\n", char_width);
+    printf("Character has index into data array:%d\n", font_outer);
 
     // Index for character data
-    alt_u8 char_data_inner_idx = 0;
+    alt_u8 font_inner = 0;
 
-    // Iterate over VRAM words
-    for(alt_u32 y_draw=y; y_draw<(y+height); y_draw++) {
-        for(alt_u32 x_draw=x; x_draw<(x+width); x_draw+=16) {
-            // Construct the word with the font data
+    // Iterate over the relevant VRAM words
+    // x_wb, y_wb are guaranteed to point to the beginning of a relevant word of memory
+    alt_u32 start_wb_x = x-(x%16);
+    for(alt_u32 y_wb=y; y_wb<(y+char_height); y_wb++) {
+        for(alt_u32 x_wb=start_wb_x; x_wb<(x+char_width); x_wb+=16) {
+
+            // Iterate through all of the pixels in the word
+            alt_u32 wy = y_wb;
             alt_u32 word = 0;
-            
-            // Iterate through the relevant pixels
-            for (alt_u8 data_x=x_draw-x; data_x < (x_draw-x+16) & data_x < width; data_x++) {
-                // Get relevant data
-                alt_u8 font_data = title_font_data[char_data_idx];
-                font_data = (font_data >> ((3-char_data_inner_idx)*2)) % 4;
+            for(alt_u32 wx=x_wb; wx < x_wb+16; wx++) {
+                // Check if the pixel is outside the character to be drawn
+                if (wx < x) continue;
 
-                // Populate word
-                word += (((alt_u32)font_data) << ((15-((data_x+x)%16))*2));
+                // Check if we are out of the width of the font
+                if (wx-x >= char_width) continue;
 
-                // Increment indices into the font data structure
-                if(++char_data_inner_idx == 4) {
-                    char_data_inner_idx = 0;
-                    char_data_idx++;
-                }
-            }
+                // Get the relevant character from the font data
+                alt_u8 font_data = title_font_data[font_outer];
+
+                // Extract the relevant pixel data
+                alt_u32 pixel_data = (font_data >> ((3-font_inner)*2)) % 4;
+
+                // Write the pixel data to the word
+                word += pixel_data << (15-(wx%16))*2;
+
+                // Increment the pixel counter for the font data
+                if (++font_inner == 4){
+                    font_inner = 0;
+                    font_outer++;
+                } 
 
             // Write the word to VRAM
-            vga_ctrl->VRAM[(x_draw/16) + (y_draw*40)] = word;
+            vga_ctrl->VRAM[(x_wb/16) + (y_wb*40)] = word;
+            }
         }
     }
+
+    // Iterate over VRAM words
+    // for(alt_u32 y_draw=y; y_draw<(y+height); y_draw++) {
+    //     for(alt_u32 x_draw=x; x_draw<(x+width); x_draw+=16) {
+    //         // Construct the word with the font data
+    //         alt_u32 word = 0;
+            
+    //         // Iterate through the relevant pixels
+    //         for (alt_u8 data_x=x_draw-x; data_x < (16-(x_draw%16)) & data_x < width; data_x++) {
+    //             // Get relevant data
+    //             alt_u8 font_data = title_font_data[char_data_idx];
+    //             font_data = (font_data >> ((3-char_data_inner_idx)*2)) % 4;
+
+    //             // Populate word
+    //             word += (((alt_u32)font_data) << ((15-((data_x+x)%16))*2));
+
+    //             // Increment indices into the font data structure
+    //             if(++char_data_inner_idx == 4) {
+    //                 char_data_inner_idx = 0;
+    //                 char_data_idx++;
+    //             }
+    //         }
+
+    //         // Write the word to VRAM
+    //         vga_ctrl->VRAM[(x_draw/16) + (y_draw*40)] = word;
+    //     }
+    // }
 }
